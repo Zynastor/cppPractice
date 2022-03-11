e1ff9c1b7b2b78b8bdee59e70ba19185324fe099
Hwang Hochan <hh1183@gmail.com>
2022-03-11 14:51:19 +0900

    Second Coomit

--- 02/2/02-1-3.cpp
+++ 02/2/02-1-3.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+
 using namespace std;
-void SwapPointer(int *(&pref1),int *(&pref2)){
-    int *ptr=pref1;
-    pref1=pref2;
-    pref2=ptr;
+
+void SwapPointer(int *(&pref1), int *(&pref2)) {
+    int *ptr = pref1;
+    pref1 = pref2;
+    pref2 = ptr;
 }
-int main(void){
-    int num1=5;
-    int *ptr1=&num1;
 
-    int num2=10;
-    int *ptr2=&num2;
+int main(void) {
+    int num1 = 5;
+    int *ptr1 = &num1;
 
-    cout<<*ptr1<<endl;
-    cout<<*ptr2<<endl;
+    int num2 = 10;
+    int *ptr2 = &num2;
 
-    SwapPointer(ptr1,ptr2);
+    cout << *ptr1 << endl;
+    cout << *ptr2 << endl;
 
-    cout<<*ptr1<<endl;
-    cout<<*ptr2<<endl;
+    SwapPointer(ptr1, ptr2);
+
+    cout << *ptr1 << endl;
+    cout << *ptr2 << endl;
     return 0;
 }
